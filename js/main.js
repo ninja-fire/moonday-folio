8268cb1e21a8bef77e47560a68181e95f97fea06
Manon Brousse <pandillamanolito@gmail.com>
2023-10-20 15:37:20 +0700

    handle form errors

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ const selectedDesignNeeds = [];
 const contactDetails = [];
 
 const dynamicForm = document.getElementById('dynamic-form');
-const formContent = document.getElementById('form-content');
+const formContentNeeds = document.getElementById('form-content-needs');
+const formContentContact = document.getElementById('form-content-contact');
 const btnContainer = document.getElementById('btn-container');
+const btnSecondary = document.getElementById('btn-back');
 const validateButton = document.getElementById('validate-wishlist-button');
 const wishlistBtn = document.getElementById('btn-wishlist');
 const btnBack = document.getElementById('btn-back');
@@ -23,86 +25,14 @@ function showContactDetailsForm() {
     // Change the main title
     document.querySelector('#dynamic-form .sticky-header h4').textContent = 'Contact Details';
 
-    // Remove the checkbox and replace it with a text input
-    // const checkboxLabel = document.querySelector('label[for="branding"]');
-    // const checkboxInput = document.getElementById('branding');
+    formContentNeeds.classList.toggle("hidden");
+    formContentContact.classList.toggle("hidden");
 
-    // Create a new paragraph element
-    const contactP = document.createElement('p');
-    contactP.className = 'caption';
-    contactP.textContent = 'Please provide your name and email so we can reach out to you. It is the first step to bringing your design project to life.';
-    contactP.style.textAlign = 'start';
-
-    // Create a new emailLabel & emailInput elements
-    const emailLabel = document.createElement('label');
-    emailLabel.textContent = 'Your email address *'
-    emailLabel.className = 'gap-1-2';
-    emailLabel.id = 'email-label';
-    emailLabel.for = 'email-details';
-
-    const emailInput = document.createElement('input');
-    emailInput.type = 'email';
-    emailInput.id = 'email-details';
-    emailInput.name = 'email-details';
-    emailInput.placeholder = 'Enter your email';
-    emailInput.pattern = '.+@globex\.com';
-    emailInput.attributes.required = 'true';
-
-    // Create a new nameLabel & nameInput elements
-    const nameLabel = document.createElement('label');
-    nameLabel.textContent = 'Your name';
-    nameLabel.className = 'gap-1-2';
-    nameLabel.id = 'name-label';
-    nameLabel.for = 'name-details';
-
-    const nameInput = document.createElement('input');
-    nameInput.type = 'text';
-    nameInput.id = 'name-details';
-    nameInput.name = 'name-details';
-    nameInput.placeholder = 'Name and Surname';
-
-    // Create a new textLabel & textInput elements
-    const textLabel = document.createElement('label');
-    textLabel.textContent = 'Describe your needs'
-    textLabel.className = 'gap-1-2';
-    textLabel.id = 'text-label';
-    textLabel.for = 'text-details';
-
-
-    const textInput = document.createElement('textarea');
-    textInput.type = 'text';
-    textInput.id = 'text-details';
-    textInput.name = 'text-details';
-    textInput.placeholder = 'Tell us more about your project and your needs...';
-    textInput.spellcheck = 'True';
-
-    const btnSecondary = document.createElement('button');
-    btnSecondary.type = 'button';
-    btnSecondary.id = 'btn-back';
-    btnSecondary.className = 'btn-secondary';
-    btnSecondary.textContent = 'Back';
-    btnSecondary.style.order = '1';
-
-    // Delete all element inside form-content
-    formContent.innerHTML = '';
-
-    formContent.appendChild(contactP);
-    btnContainer.append(btnSecondary);
-
-    formContent.appendChild(emailLabel);
-    emailLabel.appendChild(emailInput);
-
-    formContent.appendChild(nameLabel);
-    nameLabel.appendChild(nameInput);
-
-    formContent.appendChild(textLabel);
-    textLabel.appendChild(textInput);
-
-    formContent.style.height= '100%';
-    formContent.style.width= '100%';
+    btnSecondary.classList.toggle('hidden');
 
     // Update the button text and event handler
     validateButton.textContent = 'Send Us A Request';
+    validateButton.type = 'submit';
 
     validateButton.removeEventListener('click', showContactDetailsForm);
     validateButton.addEventListener('click', handleContactDetailsSubmit);
@@ -110,12 +40,32 @@ function showContactDetailsForm() {
 }
 
 // Function to handle contact details submission
-function handleContactDetailsSubmit() {
+function handleContactDetailsSubmit(e) {
+
+    e.preventDefault(); // Prevent the default form submission
+
     console.log("second click");
 
     const emailDetails = document.getElementById('email-details');
+    const emailError = document.getElementById('email-error');
     const nameDetails = document.getElementById('name-details');
     const textDetails = document.getElementById('text-details');
+
+    const emailPatternStr = ".+@.+\..+";
+
+    // Create a RegExp object from the pattern string
+    const emailPattern = new RegExp(emailPatternStr);
+
+    // Check if the email is in a valid format
+    if (!emailPattern.test(emailDetails.value)) {
+        // Display the error message below the email input
+        emailError.style.display = 'flex';
+        emailDetails.style.borderColor = '#FF8A7A';
+        return;
+    } else {
+        // Clear the error message when the input is valid
+        emailError.style.display = 'none';
+    }
 
     contactDetails.push(emailDetails.value);
     contactDetails.push(nameDetails.value);
@@ -123,25 +73,11 @@ function handleContactDetailsSubmit() {
 
     // Remove everything inside the form
     dynamicForm.innerHTML = '';
-
-    // const thanksContainer = document.createElement('div');
-    // thanksContainer.className = 'gap'
-
-    const thanksTitle = document.createElement('h4');
-    thanksTitle.textContent = "Thank you!";
-    thanksTitle.style.textAlign = 'center';
-    thanksTitle.style.width = '100%';
-
     dynamicForm.style.justifyContent = 'center';
 
-
-    const thanksP = document.createElement('p');
-    thanksP.className = 'caption';
-    thanksP.textContent = "Your message has been sent successfully. We'll get back to you as soon as possible. In the meantime, feel free to explore more of our work and services.";
-    thanksP.style.textAlign = 'center';
-
-    dynamicForm.appendChild(thanksTitle);
-    dynamicForm.appendChild(thanksP);
+    const thanksContainer = document.getElementById('thank-you-container');
+    dynamicForm.appendChild(thanksContainer);
+    thanksContainer.classList.toggle('hidden');
 
 }
 
@@ -154,7 +90,8 @@ function recreateDesignNeedsForm() {
     document.querySelector('#dynamic-form .sticky-header h4').textContent = 'Your Wish List';
 
     // Remove the contact details form elements
-    formContent.innerHTML = '';
+    formContentNeeds.classList.toggle("hidden");
+    formContentContact.classList.toggle("hidden");
 
     // Create checkboxes for design needs
     const designNeeds = [
@@ -188,9 +125,12 @@ function recreateDesignNeedsForm() {
         label.appendChild(input);
         label.appendChild(caption);
         
-        // Append the checkbox to the formContent
-        formContent.appendChild(label);
+        // Append the checkbox to the formContentNeeds
+        formContentNeeds.appendChild(label);
     });
+
+    btnSecondary.classList.toggle('hidden');
+
 
     // Update the button text and event handler to return to the contact details form
     const validateButton = document.getElementById('validate-wishlist-button');
@@ -198,9 +138,7 @@ function recreateDesignNeedsForm() {
     validateButton.removeEventListener('click', handleContactDetailsSubmit);
     validateButton.addEventListener('click', showContactDetailsForm);
 
-    const btnSecondary = document.getElementById('btn-back');
-    btnSecondary.removeEventListener('click', recreateDesignNeedsForm);
-    btnContainer.removeChild(btnSecondary);
+    // btnSecondary.removeEventListener('click', recreateDesignNeedsForm);
 
 }
 
